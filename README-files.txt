86925d332abdc1479b1ea69bd2ecbcfb26de0ffb
Jared Still <jkstill@gmail.com>
2018-02-06 10:15:44 -0800

    changed soft links to hard links for files to be included in git

---
+++ README-files.txt
@@ -0,0 +1,9 @@
+
+Just some notes.
+
+The following files are hardlinks, as softlinks do not get included in git, and I want these two files included
+
+$ ln /home/jkstill/csv-tools/csv-aggregator/csv-aggregator.pl .
+$ ln /home/jkstill/csv-tools/csv-aggregator/csv-aggregator.sh .
+
+
--- csv-aggregator.sh
+++ csv-aggregator.sh
@@ -1,1 +1,9 @@
-/home/jkstill/csv-tools/csv-aggregator/csv-aggregator.sh
+#!/bin/bash
+
+csvFile=sar-csv/sar-disk-test.csv
+
+#./csv-aggregator.pl   --key-cols timestamp --grouping-cols DEV --agg-cols tps --agg-cols 'rd_sec/s' --agg-cols 'wr_sec/s'  < $csvFile
+
+./csv-aggregator.pl --filter-cols DEV  --filter-vals 'DATA..'  --key-cols hostname --key-cols timestamp  --grouping-cols DEV  --agg-cols tps --agg-cols 'rd_sec/s' --agg-cols 'wr_sec/s'  --agg-cols 'avgrq-sz' --agg-cols 'avgqu-sz' < $csvFile
+
+
