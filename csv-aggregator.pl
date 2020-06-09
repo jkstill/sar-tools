d180b9a5d64c039ea8106fa02a69cdd8ef291e2c
Jared Still <jkstill@gmail.com>
2020-06-09 11:33:13 -0700

    added --filter-regex changed metric/device output name to the aggregate op, as using the most recent value for the column is confusing

--- csv-aggregator.pl
+++ csv-aggregator.pl
@@ -20,24 +20,33 @@ my $help = 0;
 my @groupingCols=();
 my @aggCols=();
 my @keyCols=();
+my $aggOperation='sum';
+my @aggValidOps=qw(sum avg min max);
 my @displayCols=();
 my @filterCols=();
 my @filterVals=();
+my $filterRegex=0;
 my $searchPattern=();
 my $useFilter=0;
+my $printFormat="%04.6f";
 
 GetOptions(\%optctl,
 	"delimiter=s",
 	"output-delimiter=s",
+	"print-format=s" => \$printFormat,
 	'list-available-cols!',
 	"grouping-cols=s{1,10}" => \@groupingCols,
 	"key-cols=s{1,10}" => \@keyCols,
 	"agg-cols=s{1,10}" => \@aggCols,
 	"filter-cols=s{1,}" => \@filterCols,
 	"filter-vals=s{1,}" => \@filterVals,
+	"filter-regex!" => \$filterRegex,
+	"agg-op|agg-operation=s" => \$aggOperation,
 	"debug!",
 	'help|?' => \$help, man => \$man
 ) or pod2usage(2) ;
+
+pod2usage(1) unless grep(/^${aggOperation}$/,@aggValidOps);
 
 pod2usage(1) if $help;
 pod2usage(-verbose => 2) if $man;
@@ -57,6 +66,10 @@ if ( $#filterCols != $#filterVals ) {
 	die "\nThe number of values for --filter-cols must match the number of values for --filter-vals\n";
 }
 
+if ($filterRegex and $#filterCols) { # only 1 filtercols|filtervals allowed for regex
+	die "Only 1 --filter-cols and --filter-vals allowed with --filter-regex";
+}
+
 my $hdrs=<>;
 chomp $hdrs;
 
@@ -71,7 +84,6 @@ if ($listAvailableCols) {
 	exit;
 }
 
-print "\n\@groupingCols:\n" . Dumper(\@groupingCols) if $debug;
 
 die "no grouping columns specified via --grouping-cols \n\n" unless $#groupingCols >= 0;
 # aggregate columns are those for which values are additive
@@ -88,17 +100,18 @@ my @availGroupingCols=@availDisplayCols;
 # columns that can be added
 my @availAggCols=@availDisplayCols;
 
-print "\n\@availAggCols: \n" . Dumper(\@availAggCols) if $debug;
 
-@displayCols = (@keyCols,@aggCols);
+@displayCols = (@keyCols,@groupingCols,@aggCols);
 
 if ($debug) {
-	print "\n\@keyCols \n" . Dumper(\@keyCols) . "\n";
-	print "\n\@aggCols \n" . Dumper(\@aggCols) . "\n";
-	print "\n\@groupingCols \n" . Dumper(\@groupingCols) . "\n";
+	print "\n\@availAggCols: \n" . Dumper(\@availAggCols);
+	print "\n\@availGroupingCols \n" . Dumper(\@availGroupingCols) . "\n";
 	print "\n\@displayCols \n" . Dumper(\@displayCols) . "\n";
+	print "\n\@groupingCols \n" . Dumper(\@groupingCols) . "\n";
+	print "\n\@keyCols \n" . Dumper(\@keyCols) . "\n";
+	print "\n\@groupingCols:\n" . Dumper(\@groupingCols) if $debug;
+	print "\n\@aggCols \n" . Dumper(\@aggCols) . "\n";
 }
-
 
 
 # if columns used for grouping are not already in the display list, add them
@@ -146,13 +159,18 @@ $useFilter =  ( $#filterCols >= 0 ) ? 1 : 0;
 
 if ( $useFilter ) {
 
-	my $searchPatternPFX='(?=.*(:|\b)';
-	my $searchPatternSFX='\b)';
+	if ($filterRegex) { # use the filter values as a regex
+		$searchPattern =$filterVals[0];
+	} else {
 
-	foreach my $term ( @filterVals ) {
-		# change spaces to underscore - this allows using \b word separator in pattern 
-		$term =~ s/\s/_/g;
-		$searchPattern .= "${searchPatternPFX}${term}${searchPatternSFX}{1}";
+		my $searchPatternPFX='(?=.*(:|\b)';
+		my $searchPatternSFX='\b)';
+
+		foreach my $term ( @filterVals ) {
+			# change spaces to underscore - this allows using \b word separator in pattern 
+			$term =~ s/\s/_/g;
+			$searchPattern .= "${searchPatternPFX}${term}${searchPatternSFX}{1}";
+		}
 	}
 
 	warn "Search Pattern: $searchPattern\n";
@@ -177,24 +195,26 @@ print "\%colPos:\n" , Dumper(\%colPos) if $debug;
 #    the order of metrics within a timestamp is unimportant.
 
 my %aggs=(); # aggregates
-my $firstPass=1;
+my $firstPass=1;;
 my $prevKey='';
 
 
 # print header line
 print join($outputDelimiter,@displayCols) . ',';
-print join($outputDelimiter,@aggCols) . "\n";
+print join($outputDelimiter,map{ $_ . '_' . uc($aggOperation)} @aggCols) . "\n";
 
 #exit;
+
+my $setCount=1;
 
 while(<>) {
 
 	chomp;
-	my @data=split(/$delimiter/);
+	my @line=split(/$delimiter/);
 
 	if ($useFilter) {
 		my @filterColPos = map { $colPos{$_} } @filterCols ;
-		my @searchData = @data[ @filterColPos ];
+		my @searchData = @line[ @filterColPos ];
 
 		# die if delimiter eq space, as this just will not work then
 		if ( $delimiter eq ' ') {
@@ -204,20 +224,25 @@ while(<>) {
 		# change data spaces to underscore
 		@searchData = map { s/\s/_/g; $_  } @searchData;
 		my $searchData=join(' ',@searchData);
+
+		print '#' x 80 . "\n" if $debug;
+		print "## Pattern: $searchPattern\n" if $debug;
+		print "## Search Data:\n" if $debug;
+		print "## $searchData\n" if $debug;
 		
 		if ( $searchData =~ /$searchPattern/ ) {
 			;
-			#print "## Accepting this line\n";
-			#print '## ' . join("$delimiter",@data) . "\n";
+			print "## Accepting this line\n" if $debug;
+			print '## ' . join("$delimiter",@line) . "\n" if $debug;
 		} else {
-			#print "## Rejecting this line\n";
-			#print '## ' . join("$delimiter",@data) . "\n";
+			print "## Rejecting this line\n" if $debug;
+			print '## ' . join("$delimiter",@line) . "\n" if $debug;
 			next;
 		}
 
 	}
 
-	my @setKeys = map { $data[$colPos{$_}] } @keyCols;
+	my @setKeys = map { $line[$colPos{$_}] } @keyCols;
 	my $setKey = join(':',@setKeys);
 	#print "setKey $setKey\n" if $debug;
 
@@ -225,6 +250,7 @@ while(<>) {
 		if ($firstPass) {
 			$firstPass=0;
 		} else {
+			#print "setCount: $setCount\n";
 			if ($debug) {
 				print '=' x 80, "\n";
 				print "Set Key: $prevKey\n";
@@ -240,19 +266,47 @@ while(<>) {
 				foreach my $outCol (@displayCols) {
 					print "$outputDelimiter" unless $firstCol;
 					$firstCol=0 if $firstCol;
-					print "$aggs{$aggKey}->{$outCol}";
+					# this will use the most recent value for the key column
+					# which is probably not what is wanted
+					#print "$aggs{$aggKey}->{$outCol}";
+
+					# this will print the name of the columm, which is better for aggregates
+					# it is assumed you know already what type of device is in this column
+					#print "$outCol";
+
+					# but, we do want the value of the column if it is a key column (timestamp for instance
+					if ( grep(/^${outCol}$/,@keyCols ) ) {
+						print "$aggs{$aggKey}->{$outCol}";
+					} else {
+						#print "$outCol"; # column name
+						print "$aggOperation"; # whatever aggregation op is being done - sum, min, etc
+					}
+
 				}
 				# and now the calculated columns
 				foreach my $outCol (@aggCols) {
 					print "$outputDelimiter";
-					print "$aggs{$aggKey}->{$outCol}";
+					my $outVal;
+					# already calculated
+					if ( $aggOperation =~ /^(sum|min|max)$/ ) {
+						$outVal = $aggs{$aggKey}->{$outCol};
+					} elsif ($aggOperation eq 'avg' ) {
+						$outVal = $aggs{$aggKey}->{$outCol} / $setCount;
+					} else {
+						die "Unexpected error printing output\n";
+					}
+					printf("$printFormat", $outVal);
 				}
 				print "\n";
 			}
 
+			$setCount=1;
 			%aggs=();
 		}
+	} else { 
+		$setCount++;
 	}
+
 	$prevKey=$setKey;
 
 	my @aggKeyValues;
@@ -262,14 +316,44 @@ while(<>) {
 	print "\$aggKey: $aggKey\n" if $debug;
 
 	foreach my $displayCol (@displayCols) {
-		$aggs{$aggKey}->{$displayCol} = $data[$colPos{$displayCol}];
+		$aggs{$aggKey}->{$displayCol} = $line[$colPos{$displayCol}];
 	}
 
 	foreach my $aggCol ( @aggCols ) {
-		$aggs{$aggKey}->{$aggCol} = 
-			defined $aggs{$aggKey}->{$aggCol} 
-				? $aggs{$aggKey}->{$aggCol} += $data[$colPos{$aggCol}]
-				: $data[$colPos{$aggCol}];
+		#print "aggCol: $aggCol\n";
+		#print "aggKey: $aggKey\n";
+
+		# iterate over each agg column in the line of data
+		if ( $aggOperation =~ /^(sum|avg)$/ ) {
+			$aggs{$aggKey}->{$aggCol} = 
+				defined $aggs{$aggKey}->{$aggCol} 
+					? $aggs{$aggKey}->{$aggCol} += $line[$colPos{$aggCol}]
+					: $line[$colPos{$aggCol}];
+		} elsif ( $aggOperation eq 'min' ) {
+				if ( defined $aggs{$aggKey}->{$aggCol} ) {
+					if ( 
+						( $aggs{$aggKey}->{$aggCol} > $line[$colPos{$aggCol}] )
+						&& $line[$colPos{$aggCol}] > 0
+					) {
+						$aggs{$aggKey}->{$aggCol} = $line[$colPos{$aggCol}];
+					}
+				} else {
+					$aggs{$aggKey}->{$aggCol} = $line[$colPos{$aggCol}];
+				}
+					
+		} elsif ( $aggOperation eq 'max' ) {
+				if ( defined $aggs{$aggKey}->{$aggCol} ) {
+					if ( $aggs{$aggKey}->{$aggCol} < $line[$colPos{$aggCol}] ) {
+						$aggs{$aggKey}->{$aggCol} = $line[$colPos{$aggCol}];
+					}
+				} else {
+					$aggs{$aggKey}->{$aggCol} = $line[$colPos{$aggCol}];
+				}
+					
+		} else {
+			die "unknown operation in calculations\n";
+		}
+
 	}
 
 } 
@@ -326,8 +410,17 @@ sample [options] [file ...]
    --man  full documentation
    --key-cols list of columns that define a set of data
    --grouping-cols list of columns used as a key for aggregating additive data
+	--print-format an sprintf() legal print format
    --agg-cols list of additive columns to be aggretated
+	--agg-op or --agg-operation: 
+	  aggregate operation to perform
+	  valid values are: sum min max avg
+	  default is 'sum'
+	  note: 'min' does not report values of zero
    --list-available-cols just the header line of the file will be read and available columns displayed
+	--filter-cols sar column to filter on
+	--filter-vals values to filter the column specified
+	--filter-regex specify this if --filter-vals is a regex
    --delimiter input field delimiter - default is ,
    --output-delimiter output field delimiter - default is ,
 
@@ -345,63 +438,96 @@ sample [options] [file ...]
 
 =item B<--help>
 
-Print a brief help message and exits.
+ Print a brief help message and exits.
 
 =item B<--man>
 
-Prints the manual page and exits.
+ Prints the manual page and exits.
 
-B< List All Columns from the CSV file>
+=item B<--list-available-cols>
 
  Only the header line of the file will be read and available columns displayed
 
 =item B<--key-cols>
 
-  List of columns that define a set of data
-  Data is aggregated within this set
+ List of columns that define a set of data
+ Data is aggregated within this set
 
-  example: sar data will have a time stamp. 
-  you may wish to aggregate data for all disks per each timestamp
+ example: sar data will have a time stamp. 
+ you may wish to aggregate data for all disks per each timestamp
+
+ May be repeated as often as needed.
 
 =item B<--grouping-cols>
 
  List of columns that will be used to group the data for aggregation
 
-B< Available Grouping Columns:>
-
- Simply the list of all columns
+ May be repeated as often as needed.
 
 =item B<--agg-cols>
 
  List of columns that will be additively aggregated
 
-B< Available Aggregated Columns:>
+ May be repeated as often as needed.
 
- Simply the list of all columns
+=item B<--agg-operation> or B<--agg-op>
+
+ Aggregate operation to perform
+
+ Valid values are: sum min max avg
+
+ Default is 'sum'
+
+ Note: 'min' does not report values of zero
 
 =item B<--filter-cols>
 
-The names of the column(s) used to filter the data.
+ The names of the column(s) used to filter the data.
 
-May be repeated as often as needed
+ May be repeated as often as needed
+
+ The type of aggregation specified with --agg-operation will replace the value of the device or metric being aggregated
+
+=item B<--print-format>
+
+ An sprintf() legal print format.
+
+ Default is "%04.6f"
 
 =item B<--filter-vals>
 
-The values to use with column names specified in --filter-cols 
+ The values to use with column names specified in --filter-cols 
 
-May be repeated as often as needed.
+ May be repeated as often as needed.
+
+=item B<--filter-regex>
+
+ Treat the value in --filter-vals as a standalone regex
+
+ Without this flag, some internal processing is performed on the value for --filter-vals
+
+ This option may be used only with a single --filter-vals argument
+
+ In addition, keep in mind that the regex is applied to the specified field only, not the entire line of data
+
+ The following would be used to filter particular disk devices from a CSV file of sar disk data, getting only
+ devices of major 252, minor 0-12, 14 and 17
+
+ --delimiter ','  --key-cols timestamp --grouping-cols DEV  --agg-cols tps --agg-cols 'rd_sec/s' --agg-cols 'wr_sec/s' 
+   --agg-operation sum \
+	--filter-regex --filter-cols DEV --filter-vals '^dev252-([0-9]{1}?|10|11|12|14|17)$'   < sar-csv/sar-disk.csv
 
 =item B<--list-available-cols>
 
-Read the header line of the input file, display the column names and exit
+ Read the header line of the input file, display the column names and exit
 
 =item B<--delimiter>
 
-The character used as a delimiter between output fields for the CSV input.
+ The character used as a delimiter between output fields for the CSV input.
 
 =item B<--output-delimiter>
 
-The character used as an delimiter between output fields for the CSV output.
+ The character used as an delimiter between output fields for the CSV output.
 
 
 =back
@@ -421,7 +547,9 @@ B<asm-metrics-aggregtor.pl> is used to aggregate a slice of the data output by B
 
  example: 
 
- csv-aggregator.pl   --key-cols timestamp --grouping-cols DEV --agg-cols tps --agg-cols 'rd_sec/s' --agg-cols 'wr_sec/s'  < sar-csv/sar-disk-test.csv
+ Note: the default delimiter from sadf (sar) is a semi-colon
+
+ csv-aggregator.pl --delimiter ';'  --key-cols timestamp --grouping-cols DEV --agg-cols tps --agg-cols 'rd_sec/s' --agg-cols 'wr_sec/s'  < sar-csv/sar-disk-test.csv
 
  2017-07-07 04:10:01 UTC,14717.25,209380.68,250210.78
  2017-07-07 04:20:01 UTC,18188.36,343755.49,340803.6
@@ -435,7 +563,7 @@ B<asm-metrics-aggregtor.pl> is used to aggregate a slice of the data output by B
  ...
 
 
- csv-aggregator.pl   --filter-cols DEV   --filter-vals 'DATA..' \
+ csv-aggregator.pl  --delimiter ';'  --filter-cols DEV   --filter-vals 'DATA..' \
     --key-cols hostname --key-cols timestamp \
     --grouping-cols DEV \
     --agg-cols tps --agg-cols 'rd_sec/s' --agg-cols 'wr_sec/s'  < sar-csv/sar-disk-test.csv > sar-csv/sar-disk-test-filtered.csv
